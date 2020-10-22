e5b5a550a859e421ccb0fdd70a8be94808983645
Sergey <1488@i.ua>
2020-10-22 15:34:34 +0300

    lessons 20

--- script.js
+++ script.js
@@ -126,3 +126,123 @@ if (personalMovieDB.count < 10) {
    }
 
    console.log(personalMovieDB);
+
+   let numberOfFilms;
+
+   function start() {
+      numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+      while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+         numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+      }
+   }
+   start();
+   
+   const personalMovieDB = {
+      count: numberOfFilms,
+      movies: {},
+      actors: {},
+      genres: [],
+      privat: false,
+   };
+   
+   // ---------------------------------------ДЗ 3 -------------------------
+   
+   
+   function rememberMyFilms() {
+      for (let i = 0; i < 2; i++) {
+         const a = prompt('один из последних просмотренных фильмов?', ''),
+            b = prompt('на сколько оцените его?', '');
+   
+         if (a != null && b != null && a != '' && b != null && a.length < 50) {
+            personalMovieDB.movies[a] = b;
+            console.log('done');
+         } else {
+            console.log('error');
+            i--;
+         }
+      }
+   }
+   
+   // rememberMyFilms();
+   
+   
+   function detectPersonalLevel() {
+      if (personalMovieDB.count < 10) {
+         console.log('malo');
+      } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+         console.log('norm');
+      } else if (personalMovieDB.count >= 30) {
+         console.log('a lot');
+      } else {
+         console.log('error');
+      }
+   }
+   // detectPersonalLevel();
+   
+   function showMyDB(hidden) {
+      if (!hidden) {
+         console.log(personalMovieDB);
+      }
+   }
+   
+   showMyDB(personalMovieDB.privat);
+   
+   function writeYourGeneres() {
+      for (let i = 1; i <= 3; i++) {
+         personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
+      }
+   }
+   
+   writeYourGeneres();
+
+
+   // -------------------callback------
+
+   function learnJS(lang,lang2,callback) {
+      console.log(`i lern: ${lang}`);
+      console.log(`i try speak Englesh ${lang2}`);
+      callback();
+   }
+   function done() {
+      console.log('i did it`s lesson');
+   }
+   
+   learnJS(`Java script`,`every day`, done);
+
+   // ------------------------ 20 ------------------
+   const options = {
+      name: 'test',
+      width: 1024,
+      height: 1024,
+      colors: {
+         border: 'black',
+         bg: 'red'
+      },
+      makeTest: function() {
+         console.log("Test");
+      }
+   };
+   
+   options.makeTest();
+   
+   const {border, bg} = options.colors;
+   console.log(border);
+   
+   // console.log(options.name);
+   
+   // delete options.name;
+   
+   // console.log(options);
+   
+   let counter = 0;
+   
+   for (let key in options) {
+      if (typeof(options[key]) === 'object') {
+         for (let i in options [key]) {
+            console.log (`Свойство ${i} имеет значение ${options[key][i]}`);
+         }
+      } else {
+            console.log (`Свойство ${key} имеет значение ${options[key]}`); 
+      }  
+   }
+   console.log(counter);
